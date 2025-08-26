169701434614e114b73d1ffb1fd08c979e1d0095
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-08-26 17:02:00 +0200

    add algo JS exo

---
+++ Algo JS/Turtle/algoblocs.js
@@ -0,0 +1,637 @@
+/*
+//A1
+//initialisation
+setPos(300,300);
+changeColor(color.red);
+setLineWidth(10);
+faceDown();
+//Réalisation
+forward(200);
+faceRight();
+forward(100);
+*/
+/*
+//A2
+setPos(300,300);
+changeColor(color.yellow);
+setLineWidth(10);
+faceLeft();
+//initialisation
+forward(100);
+faceDown();
+forward(100);
+faceRight();
+forward(200);
+faceUp();
+forward(200);
+//Réalisation
+*/
+/*
+//A3
+//initialisation
+setPos(300,300);
+changeColor("#FFA500");
+setLineWidth(10);
+faceLeft();
+//Réalisation
+forward(50);
+backward(100);
+forward(50);
+faceUp();
+forward(50);
+backward(100);
+*/
+/*
+//A4
+//initialisation
+setPos(200,400);
+changeColor(color.green);
+setLineWidth(10);
+faceRight();
+//Réalisation
+forward(100);
+arcLeft(50,180)
+forward(50);
+arcRight(50,180)
+forward(100);
+*/
+/*
+//A5
+//initialisation
+setPos(300,200);
+changeColor(color.fuchsia);
+setLineWidth(10);
+faceRight();
+//Réalisation
+arcRight(100,360);
+arcRight(50,180);
+arcLeft(50,180);
+*/
+/*
+//A6
+//initialisation
+setPos(300,300);
+changeColor(color.aqua);
+setLineWidth(10);
+faceUp();
+//Réalisation
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+*/
+/*
+//A7
+//initialisation
+setPos(200,300);
+changeColor(color.green);
+setLineWidth(10);
+faceDown();
+//Réalisation
+forward(100);
+arcLeft(50,180);
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+arcLeft(50,180);
+forward(50);
+*/
+/*
+//A8
+//initialisation
+setPos(300,300);
+changeColor(color.fuchsia);
+setLineWidth(10);
+faceUp();
+//Réalisation
+forward(50);
+faceRight();
+forward(100);
+faceDown();
+forward(200);
+faceLeft();
+forward(50);
+up();
+forward(50);
+faceUp();
+forward(50);
+down();
+forward(50);
+*/
+/*
+//A9
+//initialisation
+setPos(200,400);
+changeColor(color.aqua);
+setLineWidth(10);
+faceRight();
+//Réalisation
+forward(200);
+faceLeft();
+arcRight(50,180);
+arcLeft(50,180);
+forward(200);
+arcLeft(50,180);
+arcRight(50,180);
+*/
+/*
+//A10
+//initialisation
+setPos(300,300);
+changeColor(color.red);
+setLineWidth(10);
+faceRight();
+//Réalisation
+arcLeft(50,180);
+changeColor(color.green);
+arcLeft(100,180);
+changeColor(color.yellow);
+arcLeft(200,180);
+*/
+/*
+//A11
+//initialisation
+setPos(100,200);
+changeColor(color.blue);
+setLineWidth(10);
+faceUp();
+//Réalisation
+arcRight(50,180);
+faceRight();
+up();
+forward(100),
+down();
+faceUp();
+arcRight(50,180);
+up(),
+forward(50);
+faceLeft();
+forward(50);
+faceDown();
+down();
+changeColor(color.fuchsia)
+arcRight(100,180);
+*/
+/*
+//A12
+//initialisation
+setPos(50,250);
+changeColor(color.green);
+setLineWidth(10);
+faceUp();
+//Réalisation
+arcRight(50,180);
+forward(100);
+arcLeft(50,180)
+faceRight();
+up();
+forward(50);
+down();
+changeColor("#FFA500");
+faceDown();
+arcLeft(50,180);
+forward(100);
+arcRight(50,180);
+*/
+/*
+//B1
+//initialisation
+setPos(150,150);
+changeColor(color.green);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i = 0 ; i < 4 ; i++) {
+    forward(50);
+    faceDown();
+    forward(50);
+    faceRight();
+}
+*/
+/*
+//B2
+//initialisation
+setPos(50,300);
+changeColor(color.red);
+setLineWidth(10);
+faceUp();
+//Réalisation
+for(let i=0;i<4;i++){
+    forward(50);
+    faceRight();
+    forward(50);
+    faceDown(),
+    forward(50);
+    faceRight();
+    forward(50);
+    faceUp();
+}
+*/
+/*
+//B3
+//initialisation
+setPos(200,200);
+changeColor(color.fuchsia);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<4;i++){
+    forward(100);
+    right(90);
+}
+*/
+/*
+//B4
+//initialisation
+setPos(200,300);
+changeColor(color.aqua);
+setLineWidth(10);
+faceRight();
+//Réalisation
+    for(let i=0;i<3;i++){
+    forward(100);
+    left(120);
+}
+*/
+/*
+//B5
+//initialisation
+setPos(300,300);
+changeColor("#FFA500");
+setLineWidth(10);
+faceUp();
+//Réalisation
+forward(100);
+changeColor(color.red);
+for(let i=0;i<3;i++){
+    forward(50);
+    right(120)
+}    
+*/
+
+
+/*
+//B6
+//initialisation
+setPos(200,300);
+changeColor(color.red);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<2;i++){
+    forward(50);
+    faceUp();
+    forward(50);
+    faceRight();
+}
+changeColor(color.green);
+for(let i=0;i<3;i++){
+    forward(50);
+    faceDown();
+    forward(50);
+    faceRight();
+}
+*/
+/*
+//B7
+//initialisation
+setPos(300,300);
+changeColor("#FFA500");
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<3;i++){
+    forward(100);
+    left(120);
+}
+for(let i=0;i<4;i++){
+    forward(100);
+    right(90);
+}
+*/
+/*
+//B8
+//initialisation
+setPos(300,300);
+changeColor(color.yellow);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(var i=0;i<8;i++){
+    forward(200);
+    right(135);
+}
+*/
+/*
+//B9
+//initialisation
+setPos(100,300);
+changeColor(color.fuchsia);
+setLineWidth(10);
+faceRight();
+//Réalisation
+forward(100);
+up();
+forward(100);
+down();
+for(let i=0;i<3;i++){
+    forward(100);
+    right(120);
+}
+*/
+/*
+//B10
+//initialisation
+setPos(50,300);
+changeColor(color.aqua);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<8;i++){
+    forward(100);
+    right(135);
+}
+up();
+forward(200);
+down();
+changeColor(color.yellow)
+for(let i=0;i<8;i++){
+    forward(200);
+    right(135);
+}
+up();
+faceDown();
+forward(200);
+down();
+changeColor(color.fuchsia)
+for(let i=0;i<8;i++){
+    forward(100);
+    right(135);
+}
+*/
+/*
+//B11
+//initialisation
+setPos(200,200);
+changeColor(color.red);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<4;i++){
+    forward(50);
+    left(90);    
+    forward(50);
+    right(90)
+    forward(50);
+    right(90);
+}
+*/
+/*
+//B11
+//initialisation
+setPos(400,400);
+changeColor("#800080");
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<3;i++){
+    right(90);
+    forward(50);
+}
+up();
+forward(50);
+down();
+changeColor(color.green);
+for(let i=0;i<3;i++){
+    forward(50);
+    right(90);
+}
+*/
+/*
+//C1
+//initialisation
+setPos(100,300);
+changeColor(color.aqua);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<4;i++){
+    for(let j=0;j<3;j++){
+        forward(100);
+        right(120)
+    }
+    forward(100);
+}
+*/
+/*
+//C2
+//initialisation
+setPos(250,550);
+changeColor(color.fuchsia);
+setLineWidth(10);
+faceUp();
+//Réalisation
+for(let i=0;i<5;i++){
+    for(let i=0;i<4;i++){
+    forward(50);
+    right(90);
+    }
+    up(),
+    forward(100);
+    down();
+}
+*/
+/*
+//C3
+//initialisation
+setPos(50,300);
+changeColor(color.green);
+shiftColor(0.3);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<3;i++){
+    for(let j=0;j<8;j++){
+        forward(100);
+        right(135);
+    }
+    up(),
+    forward(200);
+    down();
+    shiftColor(0.2);
+}
+*/
+/*
+//C4
+//initialisation
+setPos(50,300);
+changeColor(color.red);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<5;i++){
+    for(let j=0;j<3;j++){
+        forward(50);
+        left(120);
+    }
+    changeColor("#FFA500")
+    for(let j=0;j<4;j++){
+        forward(50);
+        right(90);
+    }
+    up();
+    forward(100);
+    down();
+    changeColor(color.red);
+}
+*/
+/*
+//C5
+//initialisation
+setPos(300,300);
+changeColor(color.yellow);
+shiftColor(0.17)
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<4;i++){
+    for(let j=0;j<8;j++){
+    forward(200);
+    left(135);
+    }
+    right(90);
+    shiftColor(0.25);
+}
+*/
+/*
+//C6
+//initialisation
+setPos(300,300);
+changeColor(color.yellow);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<3;i++){
+    for(let j=0;j<5;j++){
+        forward(100);
+        left(120)
+    }
+    right(120);
+}
+*/
+/*
+//C7
+//initialisation
+setPos(300,400);
+changeColor("#FFA500");
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<8;i++){
+    for(let j=0;j<3;j++){
+        forward(50);
+        right(120);
+    }
+    forward(50);
+    left(45);
+}
+*/
+/*
+//C8
+//initialisation
+setPos(300,550);
+changeColor(color.green);
+setLineWidth(10);
+faceUp();
+//Réalisation
+forward(200);
+left(45);
+for(let i=0;i<4;i++){
+    changeColor(color.red);
+    for(let j=0;j<3;j++){
+        forward(100);
+        left(120);
+    }
+    changeColor(color.yellow)
+    forward(100);
+    right(90);
+}
+*/
+/*
+//C9
+//initialisation
+setPos(400,400);
+changeColor(color.green);
+setLineWidth(10);
+faceRight();
+//Réalisation
+for(let i=0;i<3;i++){
+    for(let j=0;j<3;j++){
+        for(let k=0;k<4;k++){
+        right(90);
+        forward(50);
+        }
+        forward(50);
+        right(120);
+    }
+    left(120);
+    forward(200);    
+}
+*/
+/*
+//D1
+//initialisation
+setPos(200,200);
+changeColor(color.red);
+setLineWidth(10);
+faceRight();
+//Réalisation
+//Affecation (déclaration + affiliation)-----
+let longueur;
+let angle;
+longueur=100
+angle=45
+//Affectation-----
+for(let i=0;i<8;i++){
+    forward(longueur)
+    right(angle)
+}
+*/
+/*
+//D10
+//initialisation
+setPos(300,300);
+changeColor(color.yellow);
+setLineWidth(10);
+faceRight();
+//Réalisation
+let lenght=20;
+for(let i=0;i<10;i++){
+    forward(lenght);
+    right(90);
+    lenght += 20;
+}
+*/
+//D4
+//initialisation
+setPos(100,550);
+changeColor(color.blue);
+setLineWidth(10);
+faceRight();
+//Réalisation
+let length=10
+let length1=20
+let lenght2=50 
+for(let i=0;i<6;i++){
+    faceUp();
+    forward(length1)
+    faceLeft();
+    forward(length1)
+    faceUp();
+    forward(length);
+    faceRight(lenght2)
+    faceUp();
+}
---
+++ Algo JS/test.js
@@ -0,0 +1,8 @@
+let lenght;
+lenght=100;
+lenght++;
+lenght += 1;
+console.log(lenght);
+let lenght2=150;
+console.log(lenght2);
+
