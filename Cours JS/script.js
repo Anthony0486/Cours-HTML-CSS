6df779a0f2657631ec95fe90b524f69a441b065e
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-09-09 12:23:08 +0200

    Add exercises 1 to 5 with console logs for variable manipulation and string concatenation

--- Cours JS/script.js
+++ Cours JS/script.js
@@ -1,14 +1,27 @@
+//Exercice 1
 let carte1 = "Tortank"
 let carte2= "Typlosion"
 console.log(carte1, carte2);
-
 let inversion = carte1;
 carte1=carte2;
 carte2=inversion;
-
 console.log(carte1, carte2);
 
+//Exercice 2
 let names = "Manu ";
 let age = "39 ans ";
 let bienvenue = "Bonjour " + names +"tu as " + age + "aujourd'hui, c'est la fiesta !";
 console.log(bienvenue);
+
+//Exercice 3
+let surname = "Antho"
+console.log(surname[4]);
+console.log(surname[surname.length-1]);
+
+//Exercice 4
+let surnames = "Anthony"
+let lastNames = "Pruniere"
+let phrase=[surnames, lastNames,surnames[0]+lastNames[0]];
+console.log(phrase);
+
+//Exercice 5
