e15ec8fcdd08b71b0b10aabb540634d61381f73b
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-09-09 10:55:45 +0200

    Add initial implementation of algorithm exam template with questions and functions

---
+++ Cours JS/script.js
@@ -0,0 +1,14 @@
+let carte1 = "Tortank"
+let carte2= "Typlosion"
+console.log(carte1, carte2);
+
+let inversion = carte1;
+carte1=carte2;
+carte2=inversion;
+
+console.log(carte1, carte2);
+
+let names = "Manu ";
+let age = "39 ans ";
+let bienvenue = "Bonjour " + names +"tu as " + age + "aujourd'hui, c'est la fiesta !";
+console.log(bienvenue);
