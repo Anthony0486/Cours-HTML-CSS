45fd4e809df79356ccf1d716f3e0bb2402264d5b
Anthony Pruniere <anthony.pruniere@wanadoo.fr>
2025-09-15 23:02:06 +0200

    Add interactive elements and styles for JS exercises in index.html and Script2.js

--- Cours JS/Exercices/Script2.js
+++ Cours JS/Exercices/Script2.js
@@ -2,7 +2,7 @@
 // document.querySelector('h4').style.background = 'yellow';
 // const baliseHTML = document.querySelector('h4');
 
-// CLICK EVENT
+// CLICK EVENTS
 const questionContainer = document.querySelector(".click-event");
 console.log(questionContainer);
 const btn1 = document.querySelector("#btn-1");
@@ -27,7 +27,7 @@ btn2.addEventListener("click", () =>{
 
 //-------------------------------------
 
-// Mouse Events
+// MOUSE EVENTS
 
 const mousemove = document.querySelector(".mousemove");
 window.addEventListener("mousemove", (e) => {
@@ -54,13 +54,68 @@ answer.addEventListener("mouseover", () => {
 
 //-------------------------------------
 
-// KEYPRESS event
+// KEYPRESS EVENTS
 
 const keypressContainer = document.querySelector(".keypress");
 const key = document.getElementById("key");
+const ring = () => {
+    const audio = new Audio();
+    audio.src = "./Assets/z.mp3";
+    audio.play();
+}
+
+
+
 document.addEventListener("keypress", (e) => {
-    console.log(e);
+key.textContent = e.key;
+    if (e.key ==="j"){
+        keypressContainer.style.background = "pink";
+    }else if (e.key === "h"){
+        keypressContainer.style.background = "teal";
+    }else {
+        keypressContainer.style.background = "red";
+        }
+ring();
+
 });
 
+//SCROLL EVENTS----------------------------
 
+const nav = document.querySelector("nav");
 
+window.addEventListener("scroll", () => {
+    console.log(window.scrollY)
+    if (window.scrollY > 120){
+        nav.style.top = 0;
+    }else {
+        nav.style.top = "-50px";
+    }
+});
+
+//INPUT EVENTS----------------------------
+const inputName = document.querySelector('input[type="text"]');
+let pseudo = "";
+const select = document.querySelector("select");
+let language = "";
+const form = document.querySelector("form");
+
+inputName.addEventListener("input", (e) => {
+    pseudo = e.target.value;
+});
+
+select.addEventListener("input", (e) => {
+    language = e.target.value;
+    
+});
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (cgv.checked){
+        document.querySelector("form > div").innerHTML = `
+        <h3>Pseudo : ${pseudo}</h3>
+        <h4>Langage préféré : ${language}</h4>
+        `
+    }else {
+        alert("Veuillez accpeter les CGV");
+    }
+
+});
